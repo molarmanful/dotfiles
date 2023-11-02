097499a0a45d77c2cd648014db82e9d0b4d05d05
molarmanful <benjinpang@gmail.com>
2023-11-02 14:20:31 -0400

    Update .config/nvim/lua/custom/plugins.lua

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -100,8 +100,6 @@ local plugins = {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
       "sindrets/diffview.nvim",
     },
     config = true,
