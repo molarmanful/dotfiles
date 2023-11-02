d14f69b79531282df273c0442a876ab6b7ade793
molarmanful <benjinpang@gmail.com>
2023-11-02 14:26:35 -0400

    Update .config/nvim/lua/custom/configs/telescope.lua

--- dot_config/nvim/lua/custom/configs/telescope.lua
+++ dot_config/nvim/lua/custom/configs/telescope.lua
@@ -1,6 +1,10 @@
 local telescope = require "telescope"
 
-telescope.setup {}
+telescope.setup {
+  defaults = {
+    border = false,
+  },
+}
 
 telescope.load_extension "fzf"
 telescope.load_extension "persisted"
