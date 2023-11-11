6d944b2779429c894cc2507e5a33fb57f6216a25
molarmanful <benjinpang@gmail.com>
2023-11-11 05:00:45 -0500

    Update .config/nvim/lua/custom/init.lua

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -2,7 +2,10 @@ local opt = vim.opt
 local g = vim.g
 local api = vim.api
 local cmd = vim.cmd
+local filetype = vim.filetype
 
 opt.scrolloff = 999
 opt.whichwrap = "b,s"
 opt.wrap = false
+
+filetype.add { extension = { sclin = "sclin" } }
