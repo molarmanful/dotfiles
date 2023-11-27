f72d6ace6c64571550cfd252768a5cee4495c00d
molarmanful <benjinpang@gmail.com>
2023-11-27 04:20:11 -0500

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/executable_wezterm.lua
+++ dot_config/wezterm/executable_wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local act = wezterm.action
 
 local config = {}
 
@@ -8,11 +9,22 @@ if wezterm.config_builder then
 end
 
 config.default_domain = "WSL:Ubuntu-22.04"
--- config.color_scheme = 'AdventureTime'
+config.webgpu_power_preference = "HighPerformance"
+config.color_scheme = "rose-pine"
+config.colors = {
+	tab_bar = { background = "#000000" },
+}
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Light" })
+config.font_size = 8
+config.cell_width = 0.8
+
+config.keys = {
+	{ key = "Backspace", mods = "CTRL", action = act.SendString("\x17") },
+}
 
 wezterm.on("gui-startup", function()
 	local window = mux.spawn_window({})
-	window:gui_window():fullscreen()
+	window:fullscreen()
 end)
 
 return config
