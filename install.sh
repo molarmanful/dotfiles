e473be273f6af0d5d1353a9a0cb620811f8bdc5e
molarmanful <benjinpang@gmail.com>
2023-11-28 14:07:41 -0500

    Update .zshrc Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 files=$(find . -maxdepth 1 -type f -name ".*")
 
-for file in $files; do
-  name=$(basename "$file")
-  echo "Creating symlink to $name in home directory."
-  rm -rf ~/"$name"
-  ln -s "$script_dir/$name" ~/"$name"
+for file in "${files[@]}"; do
+    name=$(basename "$file")
+    echo "Creating symlink to $name in home directory."
+    rm -rf ~/"$name"
+    ln -s "$script_dir/$name" ~/"$name"
 done
