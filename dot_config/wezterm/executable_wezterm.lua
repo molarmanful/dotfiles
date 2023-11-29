27792844e0994000c59542c6f216a2573cfd045b
molarmanful <benjinpang@gmail.com>
2023-11-29 03:22:09 -0500

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/executable_wezterm.lua
+++ dot_config/wezterm/executable_wezterm.lua
@@ -30,10 +30,11 @@ local font = function(fam, size, scale)
 	cfg.font_size = size
 end
 
-font({ "scientifica" }, 8, 0.7)
 font({ "TamzenForPowerline" }, 9, 0.8)
 font({ "CozetteVector" }, 9.4, 0.7)
+font({ "creep2" }, 8, 0.7)
 font("Greybeard 11px", 8, 0.8)
+font({ "scientifica" }, 8, 0.7)
 -- cfg.cell_width = 0.5
 -- cfg.line_height = 1.2
 -- cfg.freetype_load_flags = "NO_HINTING"
