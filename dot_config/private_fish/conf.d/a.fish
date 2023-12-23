1fee6191087897bc454a1a3c2465983bc208332d
molarmanful <benjinpang@gmail.com>
2023-12-23 01:48:45 -0500

    Update .config/fish/conf.d/a.fish Update .config/fish/conf.d/zzzz.fish Update .config/fish/fish_variables

--- dot_config/private_fish/conf.d/a.fish
+++ dot_config/private_fish/conf.d/a.fish
@@ -1,79 +1,79 @@
 # ASDF
 
 if not test -d ~/.asdf
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 end
 
 # SDKMAN
 
 if not test -d ~/.sdkman
-  curl -s "https://get.sdkman.io" | bash
+    curl -s "https://get.sdkman.io" | bash
 end
 
 # GHCUP
 
 if not test -d ~/.ghcup
-  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 end
 
 # NODE
 
 if not test -d ~/.volta
-  curl https://get.volta.sh | bash
+    curl https://get.volta.sh | bash
 end
 fish_add_path ~/.volta/bin
 
 if not test -e ~/.volta/bin/node
-  volta install node
+    volta install node
 end
 
 if not test -d ~/.local/share/pnpm
-  npm i -g pnpm
+    npm i -g pnpm
 end
 fish_add_path ~/.local/share/pnpm
 
 # RUST
 
 if not test -d ~/.cargo
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 end
 fish_add_path ~/.cargo/bin
 
 # NEOVIM
 
 if not test -d ~/.local/share/bob
-  cargo install bob-nvim
+    cargo install bob-nvim
 end
 fish_add_path ~/.local/share/bob/nvim-bin
 
 if not test -e ~/.local/share/bob/nvim-bin/nvim
-  bob use latest
-  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+    bob use latest
+    git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 end
 set -gx EDITOR nvim
 
 # FLY.IO
 
 if not test -d ~/.fly
-  curl -L https://fly.io/install.sh | sh
+    curl -L https://fly.io/install.sh | sh
 end
 fish_add_path ~/.fly/bin
 
 # UTIL
 
 if not test -d ~/.fzf
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
 end
 
 if not test -e ~/.cargo/bin/rg
-  cargo install ripgrep
+    cargo install ripgrep
 end
 
 if not test -e ~/.cargo/bin/fd
-  cargo install fd-find
+    cargo install fd-find
 end
 
 if not test -e ~/.cargo/bin/bat
-  cargo install bat
+    cargo install bat
 end
