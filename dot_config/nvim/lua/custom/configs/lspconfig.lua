912b1604fb43267aecfb25e796408cc3982e58f1
molarmanful <benjinpang@gmail.com>
2024-08-27 17:57:41 -0400

    Update .config/nvim/lua/custom/configs/lspconfig.lua

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,12 +4,28 @@ local configs = require "plugins.configs.lspconfig"
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
+local overrides = {
+  yamlls = {
+    settings = {
+      yaml = {
+        schemas = {
+          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        },
+      },
+    },
+  },
+}
+
 return function(lsps)
   for _, lsp in ipairs(lsps) do
-    lspconfig[lsp].setup {
+    local res = {
       on_attach = on_attach,
       capabilities = capabilities,
     }
+    for k, v in pairs(overrides[lsp] or {}) do
+      res[k] = v
+    end
+    lspconfig[lsp].setup(res)
   end
 
   require("mason-lspconfig").setup {
