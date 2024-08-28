20f6158f142e03bf9b79f8db6410516399afff7e
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-08-28 02:04:11 -0400

    Update .config/nvim/init.lua Update .config/nvim/lua/chadrc.lua Update .config/nvim/lua/configs/cmp.lua Update .config/nvim/lua/configs/conform.lua Update .config/nvim/lua/configs/lazy.lua Update .config/nvim/lua/configs/lspconfig.lua Update .config/nvim/lua/configs/mason-null-ls.lua Update .config/nvim/lua/configs/null-ls.lua Update .config/nvim/lua/configs/nvim-metals.lua Update .config/nvim/lua/configs/telescope.lua Update .config/nvim/lua/configs/treesitter.lua Update .config/nvim/lua/mappings.lua Update .config/nvim/lua/options.lua Update .config/nvim/lua/plugins/init.lua Update .config/nvim/lua/util/saveform.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,4 +1,4 @@
-require("nvchad.options")
+require 'nvchad.options'
 
 local opt = vim.opt
 local g = vim.g
@@ -8,11 +8,11 @@ local filetype = vim.filetype
 
 g.vim_markdown_folding_disabled = 1
 opt.scrolloff = 999
-opt.whichwrap = "b,s"
+opt.whichwrap = 'b,s'
 opt.wrap = false
 opt.conceallevel = 1
 
-filetype.add({ extension = { sclin = "sclin" } })
+filetype.add { extension = { sclin = 'sclin' } }
 
 -- cmd [[cab cc CodeCompanion]]
 -- cmd [[cab ccb CodeCompanionWithBuffers]]
