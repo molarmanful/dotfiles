92ae98158b9fb43449620b85b55246116475dfba
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-19 20:16:38 -0400

    Update .config/nvim/lua/configs/nvim-metals.lua

--- dot_config/nvim/lua/configs/nvim-metals.lua
+++ dot_config/nvim/lua/configs/nvim-metals.lua
@@ -9,15 +9,7 @@ return function(self)
   config.capabilities = require('cmp_nvim_lsp').default_capabilities()
 
   config.on_attach = function(client, bufnr)
-    require 'util.saveform'(client, bufnr)
-
-    for mode, v in pairs(require('core.mappings').lspconfig) do
-      if mode ~= 'plugin' then
-        for k, info in pairs(v) do
-          vim.keymap.set(mode, k, info[1])
-        end
-      end
-    end
+    require 'util.saveform' (client, bufnr)
   end
 
   local metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
